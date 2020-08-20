e793a71686b67c696fafa4325426bc1109b66ec1
Ilya Bykov <il.byckoff2011@hotmail.com>
2020-08-20 11:43:29 +0300

    Add not committed files.

---
+++ src/components/projects/MilestonesInfoPaper.js
@@ -0,0 +1,50 @@
+import React, {useContext} from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from 'prop-types';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+
+import {RealmContext} from "../../context/RealmContext";
+import MilestonesInfo from "./MilestonesInfo";
+
+const useStyles = makeStyles({
+    tableContainer: {
+        marginTop: '20px'
+    }
+});
+
+MilestonesInfoPaper.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default function MilestonesInfoPaper(props) {
+    const containerClasses = useStyles();
+    const {classes} = props;
+    const {projectWithCurrentMilestone} = useContext(RealmContext);
+
+    return (
+        <Grid container>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                    <Typography variant="h4" gutterBottom>
+                        Milestones overview
+                    </Typography>
+                    <Divider />
+
+                    {!projectWithCurrentMilestone && <div className={containerClasses.tableContainer}>
+                        <Typography variant="body1">
+                            Click on project milestone to see an overview...
+                        </Typography>
+                    </div>}
+
+                    {projectWithCurrentMilestone && <MilestonesInfo
+                        classes={containerClasses}
+                        project={projectWithCurrentMilestone}
+                    />}
+                </Paper>
+            </Grid>
+        </Grid>
+    )
+}
