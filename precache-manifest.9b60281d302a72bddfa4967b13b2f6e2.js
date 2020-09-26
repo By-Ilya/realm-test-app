b74dd70df94d3ac535903932a4825c0d04799bf9
Ilya Bykov <il.byckoff2011@hotmail.com>
2020-09-26 14:30:07 +0300

    Updates

---
+++ precache-manifest.9b60281d302a72bddfa4967b13b2f6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546e25f1c79d15a31d1c685b6a707b53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c136781fd8a3af426e62",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da997e522fb5a5255d8c",
+    "url": "/static/js/2.fd8b35cf.chunk.js"
+  },
+  {
+    "revision": "f2706737b181ff97768f84fc464af9df",
+    "url": "/static/js/2.fd8b35cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c136781fd8a3af426e62",
+    "url": "/static/js/main.a5f0c151.chunk.js"
+  },
+  {
+    "revision": "b918bb3d6b2eeff8284a",
+    "url": "/static/js/runtime-main.ac109a21.js"
+  }
+]);
